9b97738f75b17a81907389f892d8a9bd213090b9
till-m <tmuser@ethz.ch>
2021-11-04 12:09:39 +0100

    Increase training data, gap to make tests more reliable

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using Test
 
 using YaoML
 
-X, y = ad_hoc_data(20, 0.3, seed=42)
+X, y = ad_hoc_data(40, 0.5, seed=42)
 train, test = partition(eachindex(y), 0.75, seed=42+42, shuffle=true)
 
 feature_map = FeatureMaps.zz_feature_map
 model = qsvmtrain(feature_map, X[train, :], y[train])
 
-ŷ, _ = qsvmpredict(model, X[test,:])
+ŷ, decision_values = qsvmpredict(model, X[test,:])
 
 @test ŷ == y[test]
