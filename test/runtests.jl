a1011ddcfbe20d051fe3c2bd502f1bd7a6809ad2
till-m <tmuser@ethz.ch>
2022-01-10 09:28:06 +0100

    Add test for VQC

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,26 @@
 using Test
 
+import Yao
 using YaoML
 
+# load data
 X, y = ad_hoc_data(40, 0.5, seed=42)
 train, test = partition(eachindex(y), 0.75, seed=42+42, shuffle=true)
 
+# QSVM
 feature_map = FeatureMaps.zz_feature_map
 model = qsvmtrain(feature_map, X[train, :], y[train])
 
 ŷ, decision_values = qsvmpredict(model, X[test,:])
+
+@test ŷ == y[test]
+
+# VQC
+variational_map = FeatureMaps.two_local_demo
+vqc = VQC((x, theta) -> Yao.chain(feature_map(x), variational_map(theta)), [0 for i=1:16], 0.01)
+
+vqctrain!(vqc, X[train, :], y[train])
+
+ŷ = vqcpredict(vqc, X[test,:])
 
 @test ŷ == y[test]
